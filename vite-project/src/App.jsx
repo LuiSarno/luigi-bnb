5585e48720c8a96f620a6ac8d7f6540552de9eea
Luis Sarno <lulujr96@gmail.com>
2023-05-03 23:21:24 -0400

    Added a file named data.jsx that contains data for the card component. Created a function called cardElements that helps display the data onto a card.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,22 +2,44 @@ import React from "react"
 import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import Card from "./components/card"
-//import Katie from "./assets/katie.png"
+import Data from "./data"
 
 export default function App() {
+    const cardElements = Data.map(item => {
+        return   <Card 
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+    />
+    })
+    
+    
     return (
-        <div>
-            <Navbar />
-            <Hero />
-            <Card 
-            img = "katie.png"
-            rating = "5.0"
-            reviewCount = {6}
-            country = "USA"
-            title = "Life lessons with Katie Zaferes"
-            price = {136}
-            
-            />
-        </div>
-    )
+      <div>
+        <Navbar />
+        <Hero />
+        
+        
+        {cardElements}
+        
+        
+        
+        
+        
+        
+        
+        
+        {/* <Card
+          img="katie.png"
+          rating="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life lessons with Katie Zaferes"
+          price={136}
+        />  */}
+      </div>
+    );
 }
--- vite-project/src/components/card.jsx
+++ vite-project/src/components/card.jsx
@@ -10,7 +10,7 @@ export default function Card(props) {
         <img src={star} className="card--star"></img>
         <span>{props.rating}</span>
         <span className="grey">({props.reviewCount}) •</span>
-        <span className="grey">{props.country}</span>
+        <span className="grey">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p><span className="bold">From ${props.price}</span> / person</p>
